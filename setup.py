70ecbebaddf0cbfec0adeeb0519df1f299e516b7
ronaldolagepessoa <ronaldo.lage.pessoa@gmail.com>
2021-08-23 10:53:42 -0300

    version2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='Easy Apriori',
-    url='https://github.com/ronaldolagepessoa/easyapriori.git',
+    url='https://github.com/ronaldolagepessoa/easyapriori',
     author='Ronaldo Lage',
     author_email='ronaldo.lage.pessoa@gmail.com',
     # Needed to actually package something
