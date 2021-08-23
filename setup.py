3cc7ca2f01e2f00020905db44e96eac6020c5d85
ronaldolagepessoa <ronaldo.lage.pessoa@gmail.com>
2021-08-23 10:59:00 -0300

    setup2

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name='Easy Apriori',
-    url='https://github.com/ronaldolagepessoa/easyapriori',
-    author='Ronaldo Lage',
-    author_email='ronaldo.lage.pessoa@gmail.com',
-    # Needed to actually package something
-    packages=['apriori'],
-    # Needed for dependencies
-    install_requires=['pandas', 'apyori'],
-    # *strongly* suggested for sharing
-    version='1.0.0',
-    # The license can be anything you like
-    license='FREE',
-    description='Solução simples para utilização do algoritmo Apriori no python',
-    # We will also need a readme eventually (there will be a warning)
-    # long_description=open('README.txt').read(),
-)
+
+setup(name='easyapriori',
+      version='0.1.1',
+      packages = find_packages(),
+      )
